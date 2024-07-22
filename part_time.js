c7becbab17cde86b1599166254b47533568d0b8b
PRIYESH ANAND <110104047+Priyesh786@users.noreply.github.com>
2024-07-22 23:56:52 +0530

    part_time.js

--- part_time.js
+++ part_time.js
@@ -1,8 +1,11 @@
-import { attendance_report } from "./attendance.jsgi";
+import { attendance_report } from "./attendance.js";
+
+let part_time_hour = 0;
 
 if(attendance_report == 0) {
     console.log("Your part time wage is 0.")
 } else {
-    let part_time_hour = Math.floor((Math.random()*4)+1);
+    part_time_hour = Math.floor((Math.random()*4)+1);
     console.log(`Your part time_wage is ${part_time_hour*20}`)
 }
+export const part_time_work = part_time_hour;

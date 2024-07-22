fe5f70d4fb3fef6fd5045d56074d483a4598a237
Priyesh Anand <Priyesh.Anand@godigit.com>
2024-07-23 00:04:35 +0530

    UC_04

---
+++ switch.js
@@ -0,0 +1,23 @@
+import { part_time_work } from "./part_time.js";
+
+switch(part_time_work)
+{
+    case 1:
+        console.log("Your Part Time Wage is 20.");
+        break;
+    
+    case 2:
+        console.log("Your Part Time Wage is 40.");
+        break;
+    
+    case 3:
+        console.log("Your Part Time Wage is 60.");
+        break;
+    
+    case 4:
+        console.log("Your Part Time Wage is 80.");
+        break;    
+    
+    default:
+        console.log("Your Part Time Wage is 0.")
+}

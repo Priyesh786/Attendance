bd5801c3ed26ac8be0d98073664ea3b40a730372
PRIYESH ANAND <110104047+Priyesh786@users.noreply.github.com>
2024-07-23 00:21:56 +0530

    emp_wage.js

--- emp_wage.js
+++ emp_wage.js
@@ -1,11 +1,14 @@
 import { attendance_report } from "./attendance.js";
 
+let wage=0;
 if(attendance_report == 0) {
     console.log("Your daily wage is 0.")
 }
 else {
     let hours = Math.floor((Math.random()*8)+1);
-    let wage = hours*20;
+    wage = hours*20;
 
     console.log(`Your daily wage is ${wage}`);
 }
+
+export const salary = wage;
